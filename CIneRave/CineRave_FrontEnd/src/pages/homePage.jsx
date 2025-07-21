ba1c1a81ad64edbd73e5f8260803ec979e954c0d
Mukesh <tamminanamukesh7563@gmail.com>
2025-07-21 21:43:09 +0530

    cineRav

---
+++ CIneRave/CineRave_FrontEnd/src/pages/homePage.jsx
@@ -0,0 +1,14 @@
+import MoviesList from '../components/moviesList';
+import NavBar from '../components/navBar';
+
+const HomePage = ({ User, handleLogout }) => {
+  return (
+    <>
+      <NavBar User={User} handleLogout={handleLogout} />
+      <MoviesList />
+      
+    </>
+  );
+};
+
+export default HomePage;
